d0e30979ed28330cf15791375733f4ef8b7e929f
yassinejr <yassine.jrad@esprit.tn>
2021-03-03 23:30:27 +0100

    configure postgres db

--- MSMS_Django_Backend/settings.py
+++ MSMS_Django_Backend/settings.py
@@ -112,8 +112,11 @@ WSGI_APPLICATION = 'MSMS_Django_Backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        # 'HOST': localhost,
+        # 'USER':
+        # 'PASSWORD'
+        'NAME': 'postgresappdb',
     }
 }
 
